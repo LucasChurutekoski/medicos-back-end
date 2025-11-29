07192a2d90d3016ab18d0ea2dda19071c02d9e3e
Lucas Matheus Churutekoski <127150662+LucasChurutekoski@users.noreply.github.com>
2025-11-29 17:42:32 -0300

    atualização CORS

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,17 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+
+    origin: [
+      'http://localhost:3000', 
+      'http://localhost:4200', 
+      'http://localhost:8080', 
+      'http://127.0.0.1:5173', 
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, 
+  });
   app.useGlobalPipes(new ValidationPipe({
   
     whitelist: true,
